a5ba68d23bb9f9b390232cfcab3c76c84a27d7cb
ColterDowning <colterdowning0@gmail.com>
2024-10-20 18:13:15 -0700

    Correct initialization and added a data loader

--- traingpt2.py
+++ traingpt2.py
@@ -6,6 +6,7 @@
 
 from dataclasses import dataclass # For decorators
 import math
+import tiktoken
 import torch # Using PyTorch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -23,6 +24,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -61,6 +63,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -79,8 +82,8 @@ class Block(nn.Module): #sub-class of nn.Module
         self.mlp = MLP(config)
 
     def forward(self, x): # Layer norm, then attention, then another layer norm, then mlp
-        x += self.attn(self.ln_1(x))
-        x += self.mlp(self.ln_2(x))
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
         return x
 
 @dataclass # Decorator that tells Python to automatically generate special methods for the
@@ -121,7 +124,24 @@ class GPT(nn.Module): # Sub-class of the nn.Module, meaning GPT gets the methods
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # Final projection
         # from embedding space to our vocabulary (linear block in Transformer paper)
 
-    def forward(self, idx): # The inputs is our indices, our tokens
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        #init params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'): # scale to control std dev
+                std *= (2 * self.config.n_layer) ** -0.5 # mlp and attn forward for each block, so 2x
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None): # The inputs is our indices, our tokens
         # idx is of shape (B,T)
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
@@ -136,7 +156,11 @@ class GPT(nn.Module): # Sub-class of the nn.Module, meaning GPT gets the methods
         # forward the final layer norm and classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
-        return logits
+        loss = None
+        if targets is not None:
+            # The cross entropy function does not like multidimensional tensors, so we have to flatten it out to just 2 dims
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
 
     @classmethod
@@ -190,16 +214,72 @@ class GPT(nn.Module): # Sub-class of the nn.Module, meaning GPT gets the methods
         return model
     
 #----------------------------------------------------------------------------------------------------
-num_return_sequences = 5
-max_length = 30
-device = 'cuda' if torch.cuda.is_available() else 'cpu' # If a gpu is available, use it.
 
-model = GPT.from_pretrained('gpt2')
-model.eval() # When we aren't training, its best practice to put into eval mode
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        #state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch is out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+#----------------------------------------------------------------------------------------------------
+
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+print(f"using device: {device}")
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.get_device_name(0))
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=4, T=32)
+
+#model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
 model.to(device)
 
+#optimize! AdamW is a faster way to optimize compared to stochastic gradient descent
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad() # Make sure to always 0 the gradients first!
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+
+import sys; sys.exit(0)
+
 # prefix tokens
-import tiktoken
+model.eval()
 enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
